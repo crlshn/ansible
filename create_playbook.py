837894c37569f4ce33709899fe72396ee4b1dbd3
Carlos Collart <carlos.collart@gmail.com>
2024-06-25 11:19:56 -0400

    add pythoin to create commands.txt

---
+++ create_playbook.py
@@ -0,0 +1,22 @@
+with open('commandos.txt', 'r') as f:
+    commands = f.read().splitlines()
+
+    with open('playbooks/comandos.yml', 'w') as playbook:
+        playbook.write('- hosts: all\n')
+        playbook.write('  gather_facts: false\n')
+        playbook.write('  ignore_unreachable: true\n')
+        playbook.write('  tasks:\n')
+        for command in commands:
+            command_no_spaces = command.replace(' ', '_')
+            playbook.write(f'  - name: run {command} to gather evidence\n')
+            playbook.write('    ignore_errors: true\n')
+            playbook.write('    timeout: 3600\n')
+            playbook.write('    poll: 10\n')
+            playbook.write(f'    ios_command: {command}\n')
+            playbook.write(f'    register: evidence_{command_no_spaces}\n')
+        for command in commands:
+            command_no_spaces = command.replace(' ', '_')
+            playbook.write(f'  - name: display evidence {command}\n')
+            playbook.write('    debug:\n')
+            playbook.write(
+                f'      var: evidence_{command_no_spaces}["stdout_lines"][0]\n')
